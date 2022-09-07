d485d49c289e7bc6895bc1a22fd561dfdc5e80b6
Dariienko Dmytro <74827447+Dariienko@users.noreply.github.com>
2022-09-07 12:14:15 +0300

    HW3 js prompt, alert

---
+++ HW2/mian.js
@@ -0,0 +1,13 @@
+let firstNumber = prompt("Введите первое число:");
+console.log(firstNumber);
+let secondNumber = prompt("Введите второе число:");
+console.log(secondNumber);
+
+if(Number(firstNumber) + Number(secondNumber) > 20)
+{
+    alert("😊");
+}
+else
+{
+    alert("😭");
+}
