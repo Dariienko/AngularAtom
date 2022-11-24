691da157dc7023ad706aba3be11bab543341d35d
Dariienko Dmytro <74827447+Dariienko@users.noreply.github.com>
2022-11-24 19:34:28 +0200

    beers_card_angulular_edition

--- beers_card_angulular_edition/src/app/app.component.ts
+++ beers_card_angulular_edition/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'beers_card_angulular_edition';
+export class AppComponent implements OnInit{
+
+  constructor(private http: HttpClient ){}
+
+  beers: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get<any>('https://api.punkapi.com/v2/beers').subscribe(
+      (result) => this.beers = result
+    );
+  }
 }
